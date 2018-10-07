f219a20e0e64a4e61c32b56b03fe8772b3c3dd91
Joachim Dalen <me@d3v.no>
2018-10-07 21:21:32 +0200

    Add entrypoint for runner

--- src/Main.scala
+++ src/Main.scala
@@ -1,3 +1,13 @@
+import logging.Logger
+
 object Main extends App {
 
+  def run(): Unit = {
+    Logger.info("WorldCleaner starting")
+
+
+    Logger.info("WorldCleaner stopping")
+  }
+
+  run()
 }
