4d86ea0e7d9ecb9cbd102bfa0b6775d21dbda327
Joachim Dalen <me@d3v.no>
2018-12-19 08:26:36 +0100

    Use strings to preserve correct world coordinates

--- src/models/WorldItem.scala
+++ src/models/WorldItem.scala
@@ -2,7 +2,7 @@ package models
 
 import filesystem.FileSystemManager
 
-class WorldItem(val x: Int, val z: Int, val player: Player) {
+class WorldItem(val x: String, val z: String, val player: Player) {
   def getPlayerDestinationPath: String = {
     new FileSystemManager().getOrCreatePlayerFolder(this.player.uuid)
   }
