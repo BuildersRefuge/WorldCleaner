0edd653bc59a19f2a7dc9b7c1d53084db4f6870b
Joachim Dalen <me@d3v.no>
2018-12-19 07:23:59 +0100

    Add FileSystemManager (WIP)

---
+++ src/filesystem/FileSystemManager.scala
@@ -0,0 +1,96 @@
+package filesystem
+
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import config.Configuration
+import logging.Logger
+import models.Config
+
+class FileSystemManager {
+  /**
+    * The file to check for the date restriction.
+    */
+  val checkFile: String = "level.dat"
+
+  /**
+    * Get all directories in the world location directory.
+    *
+    * @return list of directory paths
+    */
+  def getDirectoriesInWorldLocation: Array[File] = {
+    val config: Config = Configuration.getConfig
+    val d = new File(config.folders.worldLocation)
+    d.listFiles().filter(p => p.isDirectory)
+  }
+
+  /**
+    * Get all directory paths for worlds that have not been
+    * modified since the match date.
+    *
+    * @return list of directory paths
+    */
+  def getDirectoriesMatchingFilter: Array[File] = {
+    val allDirectories: Array[File] = getDirectoriesInWorldLocation
+    val toProcess: Array[File] = Array()
+    for (directory <- allDirectories) {
+      if (notModifiedSinceMatchDate(directory.getAbsolutePath)) {
+        Logger.info(s"${directory.getAbsolutePath} triggers filter")
+      }
+    }
+    toProcess
+  }
+
+  /**
+    * Check if a world has not been modified since the match date.
+    *
+    * @param dir directory path for world
+    * @return true if not modified since, else false.
+    */
+  def notModifiedSinceMatchDate(dir: String): Boolean = {
+    // Check if folder contains a level.dat file.
+    val levelFile: File = new File(s"$dir/$checkFile")
+    if (!levelFile.isFile) {
+      Logger.warning(s"No level.dat file found in $dir")
+      return false
+    }
+    val math: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd")
+
+    // Get the date at last modification.
+    val lastModified: Long = levelFile.lastModified()
+    val formattedDate = math.format(lastModified)
+    val lastModifiedDate: Date = math.parse(formattedDate)
+
+    // Get the filter date from config.
+    val modifiedBefore: String = Configuration.getConfig.matchDate
+    val modifiedBeforeDate: Date = math.parse(modifiedBefore)
+
+    if (lastModifiedDate.compareTo(modifiedBeforeDate) <= 0) {
+      Logger.info(s"World at location <$dir> has not been modified after $modifiedBefore", printToConsole = true)
+      return true
+    }
+    false
+  }
+
+  /**
+    * Get or create the directory path of player folder.
+    *
+    * @param uuid uuid of the player
+    * @return path of player folder
+    */
+  def getOrCreatePlayerFolder(uuid: String): String = {
+    val parentFolder: String = Configuration.getConfig.folders.disposalLocation
+    val targetFolder: File = new File(s"$parentFolder/$checkFile")
+    if (!targetFolder.exists()) {
+      targetFolder.createNewFile()
+    }
+    targetFolder.getAbsolutePath
+  }
+
+
+  def moveFromWorldsToDisposal: Boolean = {
+
+  }
+
+}
